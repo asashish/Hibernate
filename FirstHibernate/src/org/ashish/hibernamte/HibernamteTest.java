1d9a04c72d8d90fe7caa0e846afa3eb6f5160a17
asashish <asashish147@gmail.com>
2018-03-22 05:04:38 +0530

    SecondCommit

--- FirstHibernate/src/org/ashish/hibernamte/HibernamteTest.java
+++ FirstHibernate/src/org/ashish/hibernamte/HibernamteTest.java
@@ -8,14 +8,18 @@ import org.hibernate.cfg.Configuration;
 public class HibernamteTest {
 
 	public static void main(String[] args) {
-		UserDetails user = new UserDetails();
-		user.setUserId(1);
-		user.setUserName("First User");
+		UserDetails user1 = new UserDetails();
+		user1.setUserId(1);
+		user1.setUserName("FirstUser");
+		UserDetails user2 = new UserDetails();
+		user2.setUserId(2);
+		user2.setUserName("SecondUser");
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-		session.save(user);
+		session.save(user1);
+		session.save(user2);
 		session.getTransaction().commit();
 	}
 
